173fd22cdab2ef943a99666109a1c3cafbf7c1e1
ygaros <jakub.j.jureczko@gmail.com>
2023-01-08 18:49:21 +0100

    zad-3-4

--- src/org/ygaros/javaLab/Human.java
+++ src/org/ygaros/javaLab/Human.java
@@ -1,15 +1,39 @@
 package org.ygaros.javaLab;
 
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Human {
     Animal animal;
     String name;
 
     Car car;
+    Double salary;
 
-    public Human(Animal animal, String name, Car car) {
+    public Human(Animal animal, String name, Car car, Double salary) {
         this.animal = animal;
         this.name = name;
         this.car = car;
+        this.salary = salary;
+    }
+
+    public Double getSalary() {
+        DateTimeFormatter customFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        System.out.printf("%s wartosc wyplaty %.2f\n",
+                customFormat.format(LocalDateTime.now()), this.salary);
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        if(salary <= 0){
+            return;
+        }
+        System.out.println("Nowa wysokosc wyplaty zostala wyslane do system ksiegowego");
+        System.out.println("Koniecznie potrzeba odebrac aneks do umowy od Pani Hani z kadr");
+        System.out.println("ZUS i US juz wiedza o zmianie wyplaty i nie ma sensu ukywac dochodu");
+        this.salary = salary;
     }
 
     void feedAnimal(){
--- src/org/ygaros/javaLab/Main.java
+++ src/org/ygaros/javaLab/Main.java
@@ -6,7 +6,8 @@ public class Main {
         Human hum = new Human(
                 new Animal(125d, "dog", "burek"),
                 "franek",
-                new Car("Civic", "Honda")
+                new Car("Civic", "Honda"),
+                2000d
         );
         for (int i = 0; i < 50; i++) {
             hum.animal.feed();
@@ -15,5 +16,9 @@ public class Main {
             hum.animal.takeForAWalk();
         }
         hum.animal.feed();
+        hum.getSalary();
+        hum.setSalary(1d);
+        hum.getSalary();
     }
 }
+
